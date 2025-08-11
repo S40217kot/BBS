f77ba8448787fdff16f77ef94e6ef8d7587a6595
S40217kot <sou.ok.0217@gmail.com>
2025-08-11 16:08:13 +0900

    Update soroban.py

--- pages/soroban.py
+++ pages/soroban.py
@@ -33,7 +33,7 @@ if st.sidebar.button("Home"):
     import time as ti
     with st.spinner('リダイレクト中です\nしばらくお待ちください'):
         ti.sleep(1)
-        st.switch_page("/main.py")
+        st.switch_page("")
         exit()
 
 with st.spinner('読み込み中です\nしばらくお待ちください'):
@@ -1105,5 +1105,6 @@ st.write("人差し指で球を下げます")
 st.write("一番右が一の位で左に行くにつれ二の位三の位・・・となっていきます。")
 st.write("一番上のたまが五でその下四つが一です")
 st.write("上記の方法で球を上げ下げし計算します。")
+
 
 
